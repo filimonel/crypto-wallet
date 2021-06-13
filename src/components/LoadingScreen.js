4a2b4affd4bef21e638d3ab7bcc8e3c84f96d334
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-06-13 19:29:11 +1000

    added packages - react router dom and styled components

---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const LoadingScreen = () => {
+  return <LoadingScreenWrapper></LoadingScreenWrapper>;
+};
+
+const LoadingScreenWrapper = styled.div`
+  //
+`;
+
+export default LoadingScreen;
