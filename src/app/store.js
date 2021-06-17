20384c1e3674b71ed7e6e441e2a33618034e1b28
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-06-17 21:31:48 +1000

    fixed bug - had to add providor tag in index.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
+
 // store.js the file that takes care of all the global estates and has them in this central place
 export const store = configureStore({
   reducer: {
